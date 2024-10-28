d811d80f728e0a69fa9767ba90f1dd8282bbf245
Murat <m_kairbek@outlook.com>
2024-10-28 21:23:04 +0500

    fixed: several error messages in router;

--- cmd/web/router.go
+++ cmd/web/router.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"net/http"
 	"regexp"
 	"strings"
+)
+
+// Errors
+var (
+	ErrInvalidCharacters    = errors.New("object/bucket name contains invalid characters")
+	ErrValidIPAddress       = errors.New("object/bucket name is valid IP address, IP address is not allowed")
+	ErrTooShortName         = errors.New("object/bucket name is too short, must be longer than 3 characters")
+	ErrTooLongName          = errors.New("object/bucket name is too long, must be shorter than 63 characters")
+	ErrStartWithHyphen      = errors.New("object/bucket name cannot start with hyphen or dot")
+	ErrEndWithHyphenDot     = errors.New("object/bucket name cannot end with hyphen or dot")
+	ErrConsecutiveHyphenDot = errors.New("object/bucket name has consecutive hyphens or dots")
+	ErrManySegments         = errors.New("too many segments in URL string, must be less 3")
 )
 
 func routes() *http.ServeMux {
@@ -21,60 +34,86 @@ func routes() *http.ServeMux {
 // Router handler
 func routerHandler(w http.ResponseWriter, r *http.Request) {
 	// Dividing URL into segments
-	URLString := strings.Trim(r.URL.String(), "/")
-	URLSegments := strings.Split(URLString, "/")
-
-	// URL validation
-	validateURLSegments(w, r, URLSegments)
+	URLStringTrimmed := strings.Trim(r.URL.String(), "/")
+	URLSegments := strings.Split(URLStringTrimmed, "/")
 
 	// Routing
 	switch {
-	// List buckets
+	// Index
 	case r.URL.String() == "/":
 		if r.Method == http.MethodGet {
 			getBuckets(w, r)
 			return
 		} else {
 			w.Header().Set("Allow", "GET")
-			http.Error(w, "Incorrect method applied for listing buckets", http.StatusMethodNotAllowed)
+			w.Write([]byte("Incorrect method applied for listing buckets"))
 			return
 		}
 	// Buckets operations
 	case len(URLSegments) == 1:
+		// URL validation
+		err := validateURLSegments(URLSegments)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
 		switch r.Method {
 		case "PUT":
-			createBucket(w, r, URLSegments[0])
+			err := createBucket(w, r, URLSegments[0])
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			w.Write([]byte("Created the bucket"))
 			return
 		case "DELETE":
 			deleteBucket(w, r, URLSegments[0])
+			return
 		default:
+			w.WriteHeader(http.StatusMethodNotAllowed)
 			w.Header().Set("Allow", "PUT, DELETE")
-			http.Error(w, "Incorrect method applied to operate with buckets", http.StatusMethodNotAllowed)
+			w.Write([]byte("Incorrect method entered to operate with buckets"))
 			return
 		}
 	// Objects operations
 	case len(URLSegments) == 2:
+		// URL validation
+		err := validateURLSegments(URLSegments)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
 		switch r.Method {
 		case "GET":
 			retrieveObject(w, r, URLSegments[0], URLSegments[1])
 			return
 		case "PUT":
 			createObject(w, r, URLSegments[0], URLSegments[1])
+			return
 		case "DELETE":
 			deleteObject(w, r, URLSegments[0], URLSegments[1])
+			return
 		default:
+			w.WriteHeader(http.StatusMethodNotAllowed)
 			w.Header().Set("Allow", "GET, PUT, DELETE")
-			http.Error(w, "Incorrect method applied to operate with objects", http.StatusMethodNotAllowed)
+			w.Write([]byte("Incorrect method entered to operate with objects"))
 			return
 		}
+	default:
+		http.Error(w, "Incorrect URL format entered", http.StatusBadRequest)
+		return
 	}
 
 }
 
-func validateURLSegments(w http.ResponseWriter, r *http.Request, URLSegments []string) {
+// Validation of URL variables
+func validateURLSegments(URLSegments []string) error {
 	if len(URLSegments) > 2 {
-		http.Error(w, "Too many segments in URL string, must be less 3", http.StatusBadRequest)
-		return
+		return ErrManySegments
+	} else if len(URLSegments) == 0 {
+		return nil
 	}
 
 	// regex Patterns
@@ -91,58 +130,41 @@ func validateURLSegments(w http.ResponseWriter, r *http.Request, URLSegments []s
 	checkErr(err)
 
 	// bucket and object name validation
-	for idx, segment := range URLSegments {
+	for _, segment := range URLSegments {
 		segmentBytes := []byte(segment)
-		segmentName := ""
-		if idx == 0 {
-			segmentName = "Bucket"
-		} else if idx == 1 {
-			segmentName = "Object"
-		}
 
 		// length validation
 		if len(segment) > 63 {
-			incorrectName(w, segmentName, "name too long, must not exceed 63 characters")
-			return
+			return ErrTooLongName
 		} else if len(segment) < 3 {
-			incorrectName(w, segmentName, "name too short, must not be lesser than 3 characters")
-			return
+			return ErrTooShortName
 		}
 
 		// characters validation
 		if match := CharactersPattern.Match(segmentBytes); !match {
-			incorrectName(w, segmentName, "name contains not valid characters: only lowercase letters, digits, dots, hyphens allowed")
-			return
+			return ErrInvalidCharacters
 		}
 
 		// ip validation
 		if match := IPPattern.Match(segmentBytes); match {
-			incorrectName(w, segmentName, "name matches IP address, name must not match IP addresses")
-			return
+			return ErrValidIPAddress
 		}
 
 		// begin with dot/hyphen validation
 		if match := StartHyphenDotPattern.Match(segmentBytes); match {
-			incorrectName(w, segmentName, "name must not begin with dot or hyphen")
-			return
+			return ErrStartWithHyphen
 		}
 		// end with dot/hyphen validation
 		if match := EndHyphenDotPattern.Match(segmentBytes); match {
-			incorrectName(w, segmentName, "name must not end with dot or hyphen")
-			return
+			return ErrEndWithHyphenDot
 		}
 		// consecutive dots/hyphens
 		if match := ConsecutiveDotsHyphenPattern.Match(segmentBytes); match {
-			incorrectName(w, segmentName, "name must not contain consecutive dots or hyphens")
-			return
+			return ErrConsecutiveHyphenDot
 		}
 	}
 
-}
-
-func incorrectName(w http.ResponseWriter, segmentName, message string) {
-	http.Error(w, segmentName+" "+message, http.StatusBadRequest)
-	return
+	return nil
 }
 
 func checkErr(err error) {
